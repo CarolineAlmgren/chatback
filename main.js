195a88f2fc59275dd32fbb8535c7eec4044aca32
Kajsa.Soderlund <kajsasoderlund99@gmail.com>
2024-02-22 14:48:22 +0100

    removed messages
    Co-authored-by: TmyVin <TmyVin@users.noreply.github.com>

--- main.js
+++ main.js
@@ -15,37 +15,6 @@ app.use(cors({
   credentials:true
 }));
 
-const messages = [
-  {
-    message: "What are you people doing?",
-    id: 1,
-  },
-  {
-    message: " Hey, feeling calm today. Time travel anyone?",
-    id: 2,
-  },
-  {
-    message: "who else is online?",
-    id: 3,
-  },
-  {
-    message:
-      "Im also online. And if you wonder, yes, the original - Steve Rodgers",
-    id: 4,
-  },
-  {
-    message: "Im also online. How are you people?",
-    id: 5,
-  },
-  {
-    message: "I am fine.",
-    id: 6,
-  },
-  {
-    message: "We are doing fine. I am in.",
-    id: 8,
-  },
-];
 
 app.use(session({
   secret: 'my-secret-key',
